6c0685b79f0d70038720747a29693d91469fb531
Graham Theobald <Graham_Theobald@live.co.uk>
2022-09-09 16:23:02 +0100

    mobile and desktop implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: ['./index.html'],
   theme: {
     extend: {
+      colors: {
+        Violet: 'hsl(257, 40%, 49%)',
+        SoftMagenta: 'hsl(300, 69%, 71%)',
+      },
       fontFamily: {
-        custom: ['Open Sans']
+        openSans: ['Open Sans'],
+        poppins: ['Poppins'],
       }
     },
   },
